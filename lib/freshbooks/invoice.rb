ebf914c3f747826f2634553edf13d904cc8686d5
Maxime Rousseaux-Bridle <max.rb@12grains.com>
2012-07-04 17:08:47 -0400

    Made Invoice PO Number be a string

--- lib/freshbooks/invoice.rb
+++ lib/freshbooks/invoice.rb
@@ -4,7 +4,7 @@ module FreshBooks
     
     
     define_schema do |s|
-      s.fixnum :invoice_id, :client_id, :po_number
+      s.fixnum :invoice_id, :client_id
       s.fixnum :recurring_id, :read_only => true
       s.float :amount, :discount
       s.float :amount_outstanding, :paid, :read_only => true
@@ -15,6 +15,7 @@ module FreshBooks
       s.string :number, :organization, :status, :notes, :terms, :first_name, :last_name, :currency_code
       s.string :p_street1, :p_street2, :p_city, :p_state, :p_country, :p_code
       s.string :return_uri
+      s.string :po_number
     end
     
     actions :list, :get, :create, :update, :delete, :send_by_email, :send_by_snail_mail
